59f93e43b704cbf3add3398c09257fa87b29058e
Chase Sunstrom <casunstrom+github@gmail.com>
2025-12-01 15:53:19 -0600

    I guess the pipeline doesn't want to work

--- cforge-web/.docusaurus/client-modules.js
+++ cforge-web/.docusaurus/client-modules.js
@@ -1,6 +1,6 @@
 export default [
-  require("C:\\Projects\\CPP\\cforge\\cforge-web\\node_modules\\infima\\dist\\css\\default\\default.css"),
-  require("C:\\Projects\\CPP\\cforge\\cforge-web\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
-  require("C:\\Projects\\CPP\\cforge\\cforge-web\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
-  require("C:\\Projects\\CPP\\cforge\\cforge-web\\src\\css\\custom.css"),
+  require("C:\\Projects\\CPP\\cforge-fresh\\cforge-web\\node_modules\\infima\\dist\\css\\default\\default.css"),
+  require("C:\\Projects\\CPP\\cforge-fresh\\cforge-web\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
+  require("C:\\Projects\\CPP\\cforge-fresh\\cforge-web\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
+  require("C:\\Projects\\CPP\\cforge-fresh\\cforge-web\\src\\css\\custom.css"),
 ];
